24727de9729c5390100a5f5a0ec8b15df86f4593
sundayonah <sundayonah94@gmail.com>
2023-05-04 15:36:10 +0100

    save all the config inside .env

--- src/firebase.js
+++ src/firebase.js
@@ -11,13 +11,13 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOciSAUGNniIJaXSuyxY6gCMvcZI2RaiE",
-  authDomain: "hr-intern-project.firebaseapp.com",
-  projectId: "hr-intern-project",
-  storageBucket: "hr-intern-project.appspot.com",
-  messagingSenderId: "552866664186",
-  appId: "1:552866664186:web:d48f04bc7e768bf7df4343",
-  measurementId: "G-X5SDJXSMDM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
